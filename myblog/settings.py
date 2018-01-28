712a4a22ac3b7102e8e356412829609b5438c36c
Jantz021991 <djantz@unomaha.edu>
2018-01-28 08:44:43 -0600

    Pushing changes

--- myblog/settings.py
+++ myblog/settings.py
@@ -148,3 +148,6 @@ try:
 except ImportError:
     pass
 
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
